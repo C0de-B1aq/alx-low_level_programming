01814a994b3a1048820c075426c9cdb5384425a6
Asi_the_blaq <ernestsocials@gmail.com>
2023-02-16 18:36:21 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	print("Size of a char: %lu byte(s)"), sizeof(char));
-	print("Size of an int: %lu byte(s)"), sizeof(int));
-	print("Size of a long int: %lu byte(s)"), sizeof(long int));
-	print("Size of a long long int: %lu byte(s)"), sizeof(long long int));
-	print("Size of a float: %lu byte(s)"), sizeof(float));
+	print("Size of a char: %lu byte(s)"), sizeof(char);
+	print("Size of an int: %lu byte(s)"), sizeof(int);
+	print("Size of a long int: %lu byte(s)"), sizeof(long int);
+	print("Size of a long long int: %lu byte(s)"), sizeof(long long int);
+	print("Size of a float: %lu byte(s)"), sizeof(float);
 	return (0);
 }
