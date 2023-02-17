7e425deb222eccb06a5d3d70705113d1ee814e1c
Asi_the_blaq <ernestsocials@gmail.com>
2023-02-16 17:29:53 -0800

    Printf without using puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,11 @@
  * main - This is the main function
  *
  * Return: always 0
+ *
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
