9c0068b0e0633361d42d0048cb7383fbd84a6de1
Asi_the_blaq <ernestsocials@gmail.com>
2023-02-17 20:42:43 -0800

    Hexadecimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,8 +15,8 @@ int main(void)
 	int num;
 	char ch;
 
-	for (num = 0; num < 16; num++)
-		putchar((num % 16) + '0');
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
