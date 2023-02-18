3bffe450180a6ae4780411ddaf137d77c60e71ce
Asi_the_blaq <ernestsocials@gmail.com>
2023-02-17 19:24:38 -0800

    Numbers are fun

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point function
+ *
+ * Description: 'the program prints all single digit numbers of base 10'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
