57ba651d48f753636ca0384a501e88d15d778872
Asi_the_blaq <ernestsocials@gmail.com>
2023-02-17 18:01:43 -0800

    The alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point function
+ *
+ * Description: 'the program prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+
+	putchar (ch);
+
+	return (0);
+}
