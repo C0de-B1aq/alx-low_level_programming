d00a1874a5f7508145b5f7f8c49fe36847866ee1
Asi_the_blaq <ernestsocials@gmail.com>
2023-02-23 20:04:42 -0800

    Addition in C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - prints a sum of two integers
+ *
+ * @a: a parameter for the function
+ * @b: a parameter for the function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * times_table - prints a 9 times table
+ *
+ * Description: 'the program prints _putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+
