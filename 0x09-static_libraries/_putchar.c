6274d88658e3802e50b1404cdd3ae8b806d0eaec
Asi_the_blaq <ernestsocials@gmail.com>
2023-03-13 11:08:37 -0700

    Task 0: Static Lib

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints the element of a function and display in std output.
+ *
+ * @c: int var to print
+ *
+ * Return: 1 on success
+ * If error, return -1, and errno set
+ */
+
+int _putchar(char c)
+{
+	(write(1,&c,1));
+	return 0;
+}
