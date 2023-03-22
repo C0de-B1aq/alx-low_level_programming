0283dcdef4239dd15aac2f988be7fd9226798352
Asi_the_blaq <ernestsocials@gmail.com>
2023-03-22 06:23:13 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains a dog's information
+ * @name: the first member
+ * @age: the second member
+ * @owner: The third member
+ *
+ * Description:
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
